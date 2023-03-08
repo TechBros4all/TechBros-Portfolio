f8490c74a315a1abeea34bf1ec713a2a248b9954
TechBros <126160607+TechBros4all@users.noreply.github.com>
2023-03-08 21:11:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,179 @@
+//Navbar Active
+var navBar = document.querySelector(".navbar");
+var links = document.querySelectorAll(".links a");
+
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", () => {
+        let name = links[i].innerHTML.toLowerCase();
+        let page = document.getElementById(`${name}`);
+        if (page) page.scrollIntoView();
+        else return;
+
+    })
+}
+
+//Services Spacing
+let space = 10
+
+window.onload = servicesBoxSpacing(space);
+
+window.addEventListener("resize", () => {
+    servicesBoxSpacing(space)
+})
+
+function servicesBoxSpacing(margin = 0) {
+    var servicesBox = document.querySelectorAll(".services-box");
+    var service = document.querySelectorAll(".service");
+    let width = servicesBox[0].clientWidth;
+    let spacing = 0;
+    let delay = 0;
+    for (let i = 0; i < servicesBox.length; i++) {
+        service[i].style.transitionDelay = `${delay}s`
+        servicesBox[i].style.left = `${spacing}px`;
+        spacing += (width + margin);
+        delay += 0.4;
+    }
+}
+
+//Scroll Animations
+window.addEventListener("scroll", () => {
+    navbarActive();
+    scrollAnimations()
+})
+
+
+function navbarActive() {
+    if (window.scrollY > 100) {
+        if (navBar.classList.contains("active")) return;
+        navBar.classList.add("active")
+    } else {
+        navBar.classList.remove("active")
+    }
+}
+
+function scrollAnimations() {
+    let viewport = window.innerHeight / 1.3;
+    let animate = document.querySelectorAll(".animate");
+    for (let i = 0; i < animate.length; i++) {
+        if (animate[i].getBoundingClientRect().top < viewport) {
+            if (animate[i].classList.contains("active")) continue;
+            animate[i].classList.add("active")
+        } else {
+            if (animate[i].classList.contains("active")) animate[i].classList.remove("active");
+        }
+    }
+}
+
+//Cursor follow mouse
+
+var cursor = {
+    delay: 8,
+    _x: 0,
+    _y: 0,
+    endX: (window.innerWidth / 2),
+    endY: (window.innerHeight / 2),
+    cursorVisible: true,
+    cursorEnlarged: false,
+    $dot: document.querySelector('.cursor-dot'),
+    $outline: document.querySelector('.cursor-dot-outline'),
+
+    init: function () {
+        // Set up element sizes
+        this.dotSize = this.$dot.offsetWidth;
+        this.outlineSize = this.$outline.offsetWidth;
+
+        this.setupEventListeners();
+        this.animateDotOutline();
+    },
+
+    setupEventListeners: function () {
+        var self = this;
+
+        // Anchor hovering
+        document.querySelectorAll('a').forEach(function (el) {
+            el.addEventListener('mouseover', function () {
+                self.cursorEnlarged = true;
+                self.toggleCursorSize();
+            });
+            el.addEventListener('mouseout', function () {
+                self.cursorEnlarged = false;
+                self.toggleCursorSize();
+            });
+        });
+
+        // Click events
+        document.addEventListener('mousedown', function () {
+            self.cursorEnlarged = true;
+            self.toggleCursorSize();
+        });
+        document.addEventListener('mouseup', function () {
+            self.cursorEnlarged = false;
+            self.toggleCursorSize();
+        });
+
+
+        document.addEventListener('mousemove', function (e) {
+            // Show the cursor
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+
+            // Position the dot
+            self.endX = e.pageX;
+            self.endY = e.pageY;
+            self.$dot.style.top = self.endY + 'px';
+            self.$dot.style.left = self.endX + 'px';
+        });
+
+        // Hide/show cursor
+        document.addEventListener('mouseenter', function (e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+        });
+
+        document.addEventListener('mouseleave', function (e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+        });
+    },
+
+    animateDotOutline: function () {
+        var self = this;
+
+        self._x += (self.endX - self._x) / self.delay;
+        self._y += (self.endY - self._y) / self.delay;
+        self.$outline.style.top = self._y + 'px';
+        self.$outline.style.left = self._x + 'px';
+
+        requestAnimationFrame(this.animateDotOutline.bind(self));
+    },
+
+    toggleCursorSize: function () {
+        var self = this;
+
+        if (self.cursorEnlarged) {
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+        } else {
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+        }
+    },
+
+    toggleCursorVisibility: function () {
+        var self = this;
+
+        if (self.cursorVisible) {
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+        } else {
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+        }
+    }
+}
+
+cursor.init();
